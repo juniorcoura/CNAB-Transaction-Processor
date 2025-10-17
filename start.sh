a945b4cea7b5d99b3f1bf5229ed364eaa81ab8bb
Junior Coura <jcoura2@gmail.com>
2025-10-17 15:42:44 -0300

    Docker config

---
+++ start.sh
@@ -0,0 +1,10 @@
+docker-compose down
+
+# build backend image
+docker build -t backend-pagnet:latest ./backend
+
+# build frontend image
+docker build -t frontend-pagnet:latest ./frontend
+
+# start environment
+docker-compose up --build --remove-orphans 
